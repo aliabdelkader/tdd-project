422ec50bbd239f8b1f8e98bd59698c205cea7780
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2022-05-20 19:36:08 +0200

    feat: division and multiplication features done

--- src/LearnTdd/money.py
+++ src/LearnTdd/money.py
@@ -1,6 +1,13 @@
-class Dollar():
-    def __init__(self, amount) -> None:
+class Money():
+    def __init__(self, amount: float, currency: str) -> None:
         self.amount = amount
-
+        self.currency = currency
+    
     def times(self, multiplier):
-        return Dollar(amount = self.amount * multiplier)
+        return Money(amount= self.amount * multiplier, currency=self.currency)
+    
+    def divide(self, dividor):
+        return Money(amount= self.amount / dividor, currency= self.currency)
+    
+    def __eq__(self, __o: object) -> bool:
+        return (self.amount == __o.amount) and (self.currency == __o.currency)
--- tests/test_money.py
+++ tests/test_money.py
@@ -1,13 +1,23 @@
+from locale import currency
 import unittest
 
-from LearnTdd.money import Dollar
+from LearnTdd.money import Money
 
 class TestMoney(unittest.TestCase):
-    def testMultipication(self):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEqual(10, tenner.amount)
+    def testMultipicationInDollars(self):
+        fiver = Money(amount=5, currency="USD")
+        tenner = Money(amount=10, currency="USD")
+        self.assertEqual(fiver.times(2), tenner)
     
+    def testMultiplicationInEuros(self):
+        tenEuros = Money(amount=10, currency="EUR")
+        twentyEuros = Money(amount=20, currency="EUR")
+        self.assertEqual(tenEuros.times(2), twentyEuros)
+    
+    def testDivision(self):
+        originalMoney = Money(amount=4002, currency="KRW")
+        expectedMoneyAfterDivison = Money(amount=1000.5, currency="KRW")
+        self.assertEqual(originalMoney.divide(4), expectedMoneyAfterDivison)
 
 if __name__ == "__main__":
     unittest.main()
